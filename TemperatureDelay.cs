335bba393105de948da455b1b9765d1f364a9dd7
Tudor3212 <140632886+Tudor3212@users.noreply.github.com>
2024-01-06 13:54:56 +0200

    Add files via upload

---
+++ TemperatureDelay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Engine
+
+public class Temperature : Monobehaviour
+{
+    public Health Health;
+    public int playerdamage = 2;
+    public float TemperatureCurrent = 36.6f;
+    public float temperatureNormal = 36.6f;
+    public float temperatureCritical = 34f;
+    public float freezeSpeed = 0.05f
+    public float freezedamageTimer = 1;
+    public float freezedamagedelay = 2;
+
+    void Update()
+    {
+        TemperatureCurrent -= freezeSpeed * Time.deltatime}:
+            
+              if (TemperatureCurrent <= TemperatureCritical)
+            {
+                     if (freezedamageTimer <= 0)
+                     {
+                            health.TakeDamage(PlayerDamage):
+                            freezedamageTimer += freezedamagedelay:
+                     }
+                     else
+                     {
+                            freezedamageTimer -= Time.deltatime;
+                     }
+            }
+    }
+}
